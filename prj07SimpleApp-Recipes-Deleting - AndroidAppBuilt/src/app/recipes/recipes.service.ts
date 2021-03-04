e85bae05ee31ea2c8e036d85efd4d732a62bf410
Tahir Jadoon <tjadoon@gmail.com>
2021-03-04 00:34:54 -0500

    Creating a native android app

---
+++ prj07SimpleApp-Recipes-Deleting - AndroidAppBuilt/src/app/recipes/recipes.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private recipes: Recipe[] = [{
+    id: 'r1',
+    title: 'Schnitzel',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Schnitzel.JPG/1024px-Schnitzel.JPG',
+    ingredients: ['French Fries', 'Pork Meat', 'Salad']
+  },
+  {
+    id: 'r2',
+    title: 'Spaghetti',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/1024px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+  }];
+
+  constructor() { }
+
+  getAllRecipes(){
+    //using the spread operator, pull all the recipes and return a new instance of the array which is a copy.
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string){
+    return {
+      ...this.recipes.find(x => {
+        return x.id === recipeId;
+      })
+    };
+  }
+
+  deleteRecipe(recipeId: string){
+    this.recipes = this.recipes.filter(r => {
+      return r.id !== recipeId;
+    });
+
+  }
+
+}
