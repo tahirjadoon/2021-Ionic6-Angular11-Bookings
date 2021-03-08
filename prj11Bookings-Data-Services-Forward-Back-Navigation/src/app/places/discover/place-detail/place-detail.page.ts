943faa1e56584b74f3a375cb475cabecd841ccbc
Tahir Jadoon <tjadoon@gmail.com>
2021-03-08 01:50:54 -0500

    Bookings Data Services and Forward/Back Navigation

---
+++ prj11Bookings-Data-Services-Forward-Back-Navigation/src/app/places/discover/place-detail/place-detail.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-place-detail',
+  templateUrl: './place-detail.page.html',
+  styleUrls: ['./place-detail.page.scss'],
+})
+export class PlaceDetailPage implements OnInit {
+
+  constructor(private router: Router, private navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  onBookPlace(){
+    //this.router.navigateByUrl('/places/tabs/discover'); //displays forward animation
+    this.navCtrl.navigateBack('/places/tabs/discover'); //displays back animation
+    //this.navCtrl.pop(); //pop the last page of the stack, unreliable.
+  }
+}
