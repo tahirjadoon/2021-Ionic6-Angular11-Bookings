505e94675c0b2574a7e419249b993c054ceff71e
Tahir Jadoon <tjadoon@gmail.com>
2022-01-18 02:42:05 -0500

    download

--- README.md
+++
@@ -1,8 +0,0 @@
-"# 2021-Ionic6-Angular11-Bookings" 
-
-
-Check following for more details 
-
-1. 00ProjectSetup.txt
-2. 01IonicBasics.txt
-3. 02AngularCommands.txt
--- prj18Bookings-virtual-scrolling/src/app/Utilities/Models/booking.model.ts
+++
@@ -1,3 +0,0 @@
-export class Booking {
-  constructor(public id: string, public placeId: string, public userId: string, public placeTitle: string, public guestNumber: number){}
-}
